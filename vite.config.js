d1ceeba26c105fec3f10b28e1a94318ee214a146
yuvrajshrirame <mail.yuvrajshrirame@gmail.com>
2026-07-04 23:20:39 +0530

    chore(env): configure dev environment for spotify api

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '127.0.0.1', // Forces Vite to listen on the IPv4 address
+    port: 5173,
+  }
 })
